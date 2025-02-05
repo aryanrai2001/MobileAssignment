0c727a94bef781fad41204ee82bd1ec5f5a03a1b
Aryan Rai <aryanrai3112@gmail.com>
2025-02-05 12:49:37 +0530

    Search Functionality Implemented

--- Assignment/ui/DevicesList.swift
+++ Assignment/ui/DevicesList.swift
@@ -10,16 +10,40 @@ import SwiftUI
 struct DevicesList: View {
     let devices: [DeviceData]
     let onSelect: (DeviceData) -> Void // Callback for item selection
+    
+    @State private var searchText: String = ""
+    @State private var filteredDevices: [DeviceData] = []
 
     var body: some View {
-        List(devices) { device in
-            Button {
-                onSelect(device)
-            } label: {
-                VStack(alignment: .leading) {
-                    AssignmentText(text: device.name)
+        VStack {
+            TextField("Search", text: $searchText)
+                .padding()
+                .onChange(of: searchText) { oldValue, newValue in
+                    if !newValue.isEmpty {
+                        filteredDevices = devices.filter({ deviceData in
+                            deviceData.name.lowercased().hasPrefix(newValue.lowercased())
+                        })
+                    } else {
+                        filteredDevices = devices
+                    }
                 }
+            
+            if !filteredDevices.isEmpty {
+                List(filteredDevices) { device in
+                    Button {
+                        onSelect(device)
+                    } label: {
+                        VStack(alignment: .leading) {
+                            AssignmentText(text: device.name)
+                        }
+                    }
+                }
+            } else {
+                Text("No Device Found!")
             }
+        }
+        .onAppear {
+            filteredDevices = devices
         }
     }
 }
